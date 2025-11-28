8cf2cb17e4004d48e8ed10233e533a21b242349f
Mohit Urkude <mohiturkude15@gmail.com>
2025-11-28 14:18:37 +0530

    new code update

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
-require('dotenv').config();
-const app = require('./app');
-const { sequelize } = require('./models');
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const helloRouter = require('./routes/hello')
 
-const PORT = process.env.PORT || 5000;
+app.use(express.json())
 
-(async () => {
-  try {
-    await sequelize.authenticate();
-    await sequelize.sync();
-    console.log('Database Connected ✔');
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  } catch (err) {
-    console.error('DB Connection Error:', err);
-    process.exit(1);
-  }
-})();
+app.use('/hello',helloRouter)
+
+app.get('/',(req,res)=>{
+    res.send('Welcome to Simple Node.js on Azure!')
+})
+
+const PORT = process.env.PORT || 3002
+app.listen(PORT,()=>console.log(`Server running on port ${PORT}`))
